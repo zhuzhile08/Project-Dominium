41c49351f09ab0f354490ddaaef37a0f1c6cd1ad
finncibis <finncibis614@gmail.com>
2023-10-09 15:06:48 +0200

    Draw rectangle test

--- src/main.lua
+++ src/main.lua
@@ -1,23 +1,13 @@
+function love.load()
+    love.graphics.setBackgroundColor(1, 0.2, 0) -- Set background color to orange
+end
 
+function love.draw()
+    love.graphics.setColor(1, 0.2, 0)
+    love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    love.graphics.setColor(1, 1, 0)
+    love.graphics.rectangle("fill", 0, 0, 20, 20)
+end
 
