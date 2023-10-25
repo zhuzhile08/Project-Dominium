1bdee13045950be7f79c089e0cfd17cce9991289
Zhile Zhu <zhuzhile08@gmail.com>
2023-10-25 20:33:18 +0200

    Use vectors instead of transform

--- src/Entities/entity.lua
+++ src/Entities/entity.lua
@@ -1,10 +1,14 @@
+local vec2 = require "vector2"
+
 local entity = { }
 entity.__index = entity
 
 function entity.new(posX, posY, rot, scaleX, scaleY)
 	local self = setmetatable({}, entity)
 
-	self.transform = love.math.newTransform(posX, posY, rot, scaleX, scaleY)
+	self.translation = vec2.new(posX, posY)
+	self.rotation = rot
+	self.scale = vec2.new(scaleX, scaleY)
 
 	return self
 end
