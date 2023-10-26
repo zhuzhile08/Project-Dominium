f3cbd0ecb496e031afd100d1c542bc3dbd395197
Zhile Zhu <zhuzhile08@gmail.com>
2023-10-26 21:02:33 +0200

    Add function to generate quads

--- src/init.lua
+++ src/init.lua
@@ -1,49 +1,68 @@
 require "utility"
 require "Tilemap.tilemap"
-require "input"
 
 local vec2 = require "vector2"
 local camera = require "Entities.camera"
 local player = require "Entities.player"
 
 function loadAssets()
-	love.graphics.setDefaultFilter("nearest", "nearest")
+	local function generateQuads(texture, cellSize)
+		cellSize = cellSize or 18
+
+		local r = { }
+
+		for x = 1, texture:getWidth() * texture:getHeight() / cellSize * cellSize, 1 do
+			r[x] = love.graphics.newQuad(
+				((x - 1) % (texture:getWidth() / cellSize)) * cellSize + 1, 
+				math.floor((x - 1) / (texture:getWidth() / cellSize)) * cellSize + 1, 
+				16, 
+				16, 
+				texture:getWidth(), 
+				texture:getHeight()
+			)
+		end
+
+		return r
+	end
+
 	GroundTilesetTexture = love.graphics.newImage("data/Image/GroundTileset.png")
+	GroundTilesetQuads = generateQuads(GroundTilesetTexture)
+
 	WaterTilesetTexture = love.graphics.newImage("data/Image/WaterTileset.png")
+	WaterTilesetQuads = generateQuads(WaterTilesetTexture)
+	
+	SpriteSheetTexture = love.graphics.newImage("data/Image/SpriteSheet.png")
+	SpriteSheetQuads = generateQuads(SpriteSheetTexture)
 end
 
 function initGameComponents()
 	Camera = camera.new(0, 0, 0, GraphicsGlobalScale, GraphicsGlobalScale)
-	Player = player.new(0, 0, 0, 1, 1, 300)
+	Player = player.new(0, 0, 0, 1, 1, SpriteSheetQuads[87], 75)
 
 	Camera:target(Player)
 end
 
 function love.load()
-	GraphicsGlobalScale = 2
+	GraphicsGlobalScale = 3
 
-	TilemapWidth = 50
-	TilemapHeight = 50
+	TilemapWidth = 80
+	TilemapHeight = 80
+
+	love.graphics.setDefaultFilter("nearest", "nearest")
 
 	-- log file
 	LogFile = assert(io.open("src/log.txt", "w"))
 
 	loadAssets()
-	initInputSystems()
 	initGameComponents()
 	initTilemapSystem()
 
-	--[[
-
 	local i = 1
-	while i < 50 do
+	local seed = os.time()
+	while i < 10 do
 		s, _ = pcall(function() 
 			i = i + 1
-			Tilemap = generateTilemap(os.time(), 100, 100)
+			Tilemap = generateTilemap(seed + i, TilemapWidth, TilemapHeight)
 		end)
 	end
-
-	--]]
-
-	Tilemap = generateTilemap(os.time(), TilemapWidth, TilemapHeight)
 end
