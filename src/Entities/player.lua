099c12559dc68244efb24b090e684ba5102a9667
Zhile Zhu <zhuzhile08@gmail.com>
2023-10-26 21:01:37 +0200

    Fully implement player class

--- src/Entities/player.lua
+++ src/Entities/player.lua
@@ -1,36 +1,58 @@
 local entity = require "Entities.entity"
 local vec2 = require "vector2"
-local key = require "input"
 
 local player = { }
 player.__index = player
 
-function player.new(posX, posY, rot, scaleX, scaleY, speed)
+function player.new(posX, posY, rot, scaleX, scaleY, quad, speed)
     local self = entity.new(posX, posY, rot, scaleX, scaleY)
 
 	self.speed = speed
+	self.quad = quad
 
-    self.up = key.new("w", function(dt)
-		self.translation.y = self.translation.y + self.speed * dt
-	end)
-
-	self.down = key.new("s", function(dt)
-		self.translation.y = self.translation.y - self.speed * dt
-	end)
-
-	self.left = key.new("a", function(dt)
-		self.translation.x = self.translation.x + self.speed * dt
-	end)
-
-	self.right = key.new("d", function(dt)
-		self.translation.x = self.translation.x - self.speed * dt
-	end)
+	function self:draw()
+		love.graphics.draw(
+			SpriteSheetTexture, 
+			quad,
+			self.translation.x,
+			self.translation.y,
+			0,
+			self.scale.x,
+			self.scale.y,
+			8,
+			8
+		)
+	end
 
     function self:update(dt)
-        KeyboardInputSystem:add(self.up)
-        KeyboardInputSystem:add(self.down)
-        KeyboardInputSystem:add(self.left)
-        KeyboardInputSystem:add(self.right)
+		dx = 0
+		dy = 0
+
+		if love.keyboard.isDown("w") then
+			dy = -1
+		end
+		if love.keyboard.isDown("s") then
+			dy = 1
+		end
+
+        if love.keyboard.isDown("d") then
+			dx = 1
+			self.scale.x = 1
+		end
+		if love.keyboard.isDown("a") then
+			dx = -1
+			self.scale.x = -1
+		end
+
+		if dx ~= 0 and dy ~= 0 then
+			dx = dx * 0.70711
+			dy = dy * 0.70711
+		end
+
+		self.translation.x = self.translation.x + dx * dt * self.speed
+		self.translation.y = self.translation.y + dy * dt * self.speed
+
+		fprint(self.translation.x, "\n")
     end
 
     return self
