00f0a385aa3acd7298984c7e07af143906fb4e96
Zhile Zhu <zhuzhile08@gmail.com>
2023-11-06 15:43:57 +0100

    Update soliders

--- src/main.lua
+++ src/main.lua
@@ -5,10 +5,16 @@ function love.draw()
     Camera:beginDraw()
     drawTilemap(Tilemap)
     Player:draw()
+    Solider:draw()
+    Solider1:draw()
+    Solider2:draw()
     Camera:endDraw()
 end
 
 function love.update(dt)
     Player:update(dt)
+    Solider:update(dt)
+    Solider1:update(dt)
+    Solider2:update(dt)
     Camera:update(dt)
 end
